9ff7e0e5f71194ea2db1aeacb3371676b778a0c9
morgancundiff <mcundiff@ucsd.edu>
2014-06-01 21:41:00 -0700

    Update ItemsTableSeeder.php

--- app/database/seeds/ItemsTableSeeder.php
+++ app/database/seeds/ItemsTableSeeder.php
@@ -24,7 +24,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 1,
                                         "title" => "Espresso",
-                                        "description" => "",
+                                        "description" => "Concentrated drink consisting of ground coffee and hot water.  A shot is commonly used to measure espresso which each shot consisting of one ounce of espresso.",
                                         "ratings_id" => 1
                                 ),
                         array(
@@ -41,7 +41,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 1,
                                         "title" => "Americano",
-                                        "description" => "Espresso with Water",
+                                        "description" => "1/3 part Espresso and 2/3 parts hot water.  The water adds volume (decreasing concentration) to the caffeine.",
                                         "ratings_id" => 3
                                 ), 
 
@@ -50,7 +50,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 1,
                                         "title" => "Cafe au Lait",
-                                        "description" => "Half Coffee, half steamed milk, topped with creamy foam",
+                                        "description" => "Half coffee, half steamed milk, topped with creamy foam which makes sense given the name means 'Coffee with Foam' in French",
                                         "ratings_id" => 4
                                 ), 
                         array(
@@ -58,7 +58,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 1,
                                         "title" => "Hammerhead",
-                                        "description" => "hot coffee & espresso - serious medicine!",
+                                        "description" => "A shot of Espresso in a cup of hot coffee- serious medicine! (can also be called a red eye)",
                                         "ratings_id" => 5
                                 ),
                         array(
@@ -66,7 +66,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 1,
                                         "title" => "Cappuccino",
-                                        "description" => "Cafe Latte with less milk & more foam",
+                                        "description" => "1/3 espresso, 1/3 steamed milk, and 1/3 frothed milk or microfoam though some places choose to add more milk to increase volume",
                                         "ratings_id" => 6
                                 ),
 
@@ -75,7 +75,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 1,
                                         "title" => "Cafe Mocha",
-                                        "description" => "espresso, steamed milk & Ghiradelli chocolate, topped with whipped cream",
+                                        "description" => "Espresso, steamed milk & Ghiradelli chocolate, topped with whipped cream. (though other locations may just make a cafe late with chocolate syrup)",
                                         "ratings_id" => 7
                                 ), 
 
@@ -84,15 +84,15 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 1,
                                         "title" => "Cafe Latte",
-                                        "description" => "esspresso & steamed milk, topped with creamy foam",
+                                        "description" => "Esspresso & steamed milk, topped with creamy foam.",
                                         "ratings_id" => 8
                                 ),
                         array(
                                         "id" => 9,
                                         "menus_id" => 1,
                                         "category_id" => 1,
-                                        "title" => "Wjite Chocolate Mocha",
-                                        "description" => "espresso, steamed milk & Ghiradelli white chocolate, topped with whipped cream",
+                                        "title" => "White Chocolate Mocha",
+                                        "description" => "Espresso, steamed milk & Ghiradelli white chocolate, topped with whipped cream",
                                         "ratings_id" => 9
                                 ),
                         array(
@@ -100,7 +100,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 1,
                                         "title" => "Mexican Mocha",
-                                        "description" => "espresso, steamed milk & Ibara chocolate, topped with whipped cream & cinnamon",
+                                        "description" => "Espresso, steamed milk & Ibara chocolate, topped with whipped cream & cinnamon",
                                         "ratings_id" => 10
                                 ),
 
@@ -109,7 +109,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 1,
                                         "title" => "Breve Latte",
-                                        "description" => "espresso & steamed half & half",
+                                        "description" => "1/2 Espresso & 1/2 steamed milk",
                                         "ratings_id" => 11
                                 ), 
 
@@ -118,7 +118,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 2,
                                         "title" => "Hot Tea",
-                                        "description" => "variety of black, gree & herbal teas",
+                                        "description" => "Your choice of black, green or herbal tea",
                                         "ratings_id" => 12
                                 ), 
                         array(
@@ -159,8 +159,8 @@ class ItemsTableSeeder extends Seeder {
                                         "id" => 17,
                                         "menus_id" => 1,
                                         "category_id" => 3,
-                                        "title" => "Americano",
-                                        "description" => "",
+                                        "title" => "Iced Americano",
+                                        "description" => "1/3 part Espresso and 2/3 parts water put on ice.  The water adds volume to (and decreses the concentration of) the caffeine.",
                                         "ratings_id" => 17
                                 ), 
 
@@ -169,7 +169,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 3,
                                         "title" => "Iced latte or cappuccino",
-                                        "description" => "",
+                                        "description" => "Your favorite latte (espresso and steamed milk) or cappuccino (espresso, steamed milk, and frothed milk)",
                                         "ratings_id" => 18
                                 ),
                         array(
@@ -177,7 +177,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 3,
                                         "title" => "Iced Mocha",
-                                        "description" => "",
+                                        "description" => "Espresso, steamed milk & Ghiradelli chocolate, topped with whipped cream put on ice.",
                                         "ratings_id" => 19
                                 ),
                         array(
@@ -185,7 +185,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 3,
                                         "title" => "Spice on Ice",
-                                        "description" => "Chai Tea Latte",
+                                        "description" => "Iced Chai Tea Latte: Sweet, spicy black tea & steamed milk on ice.",
                                         "ratings_id" => 20
                                 ),
 
@@ -194,7 +194,7 @@ class ItemsTableSeeder extends Seeder {
                                         "menus_id" => 1,
                                         "category_id" => 3,
                                         "title" => "Iced White",
-                                        "description" => "White Chocolate Mocha",
+                                        "description" => "White Chocolate Mocha: Espresso, steamed milk & Ghiradelli white chocolate, topped with whipped cream on ice",
                                         "ratings_id" => 21
                                 ), 
 
