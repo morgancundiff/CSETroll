3eeb443c515149e6623c903d8d45242813ce144b
morgancundiff <mcundiff@ucsd.edu>
2014-06-01 21:04:53 -0700

    Update Location Seeds (8 total locations)
    Added GPS coordinates and name for each of the additional shops.
    Added location id and menu id associated with each shop, which may cause updates needed elsewhere.
    Added minimal comments to the file.

--- app/database/seeds/LocationsTableSeeder.php
+++ app/database/seeds/LocationsTableSeeder.php
@@ -2,12 +2,18 @@
 
 class LocationsTableSeeder extends Seeder {
 
+
+	/* Populate the database with the locations of the known coffee shops.
+	 * Each array has name, id number, address, latitude, longitude, hours, and menu associated with the item.
+	 */
 	public function run() {
 		
 			DB::table('locations')->delete();
         		   
                 DB::table('locations')->insert(
+                	//Dynamically create an array of shop arrays
                         array(
+                        	//Dynamically create an array with a single shop's information: Fairbanks Warren
                                 array(
                                 	"location_name" => "Fairbanks - Warren Lecture Hall",
                                 	"id" 			=> 1,
@@ -17,7 +23,8 @@ class LocationsTableSeeder extends Seeder {
                                 	"hours"			=> '{"hours":{"open":{"Sunday":"closed","Monday":"7:00am","Tuesday":"7:00am","Wednesday":"7:00am","Thursday":"7:00am","Friday":"7:00am","Saturday":"closed"},"close":{"Sunday":"closed","Monday":"5:00pm","Tuesday":"5:00pm","Wednesday":"5:00pm","Thursday":"5:00pm","Friday":"5:00pm","Saturday":"closed"}}}',
                                 	"menus_id"		=> 1,
                                 	),   
-                               
+                                	
+                                //Dynamically create an array with a single shop's information: Fairbanks Center
                                 array(
                                 	"location_name" => "Fairbanks - Center Hall",
                                 	"id" 			=> 2,
@@ -27,7 +34,8 @@ class LocationsTableSeeder extends Seeder {
                                 	"hours"			=> '{"hours":{"open":{"Sunday":"closed","Monday":"7:00am","Tuesday":"7:00am","Wednesday":"7:00am","Thursday":"7:00am","Friday":"7:00am","Saturday":"closed"},"close":{"Sunday":"closed","Monday":"5:00pm","Tuesday":"5:00pm","Wednesday":"5:00pm","Thursday":"5:00pm","Friday":"5:00pm","Saturday":"closed"}}}',
                                 	"menus_id"		=> 1,
                                 	), 
-
+				
+				//Dynamically create an array with a single shop's information: Fairbanks Hopkins
                                 array(
                                 	"location_name" => "Fairbanks - Hopkins Parking Structure",
                                 	"id" 			=> 3,
@@ -38,6 +46,7 @@ class LocationsTableSeeder extends Seeder {
                                 	"menus_id"		=> 1,
                                 	) 
                                 	
+                                //Dynamically create an array with a single shop's information: Fairbanks BioMed
                                 array(
                                 	"location_name" => "Fairbanks - BioMedical Building",
                                 	"id" 			=> 4,
@@ -47,7 +56,8 @@ class LocationsTableSeeder extends Seeder {
                                 	"hours"			=> '{"hours":{"open":{"Sunday":"closed","Monday":"7:00am","Tuesday":"7:00am","Wednesday":"7:00am","Thursday":"7:00am","Friday":"7:00am","Saturday":"closed"},"close":{"Sunday":"closed","Monday":"5:00pm","Tuesday":"5:00pm","Wednesday":"5:00pm","Thursday":"5:00pm","Friday":"5:00pm","Saturday":"closed"}}}',
                                 	"menus_id"		=> 1,
                                 	) 
-                                	
+                                
+                                //Dynamically create an array with a single shop's information: Fairbanks Revelle
                                 array(
                                 	"location_name" => "Fairbanks - Revelle Plaza",
                                 	"id" 			=> 5,
@@ -58,6 +68,7 @@ class LocationsTableSeeder extends Seeder {
                                 	"menus_id"		=> 1,
                                 	) 
                                 	
+                                //Dynamically create an array with a single shop's information: Art of Espresso
                                 array(
                                 	"location_name" => "Art of Espresso - Mandeville",
                                 	"id" 			=> 6,
@@ -68,6 +79,7 @@ class LocationsTableSeeder extends Seeder {
                                 	"menus_id"		=> 2,
                                 	) 
                                 	
+                                //Dynamically create an array with a single shop's information: Perks
                                 array(
                                 	"location_name" => "Perks - Bookstore",
                                 	"id" 			=> 7,
@@ -78,6 +90,7 @@ class LocationsTableSeeder extends Seeder {
                                 	"menus_id"		=> 3,
                                 	) 
                                 	
+                                //Dynamically create an array with a single shop's information: Cups
                                 array(
                                 	"location_name" => "Cups - Engineering Complex",
                                 	"id" 			=> 8,
